ce62f78638356b3d50ceff9cd423e109527fe40d
ranjithkumar404 <ranjithkumarshetty808@gmail.com>
2023-05-17 22:11:31 +0530

    new commit

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -8,8 +8,8 @@ const Tour = ({ tours }) => {
                 tours.map((i) => {
                     const { id, name, info, image, price } = i;
                     return (
-                        <div key={id} className='text-black grid w-[550px] p-5 shadow-xl mt-4 gap-4 rounded-xl' >
-                            <img className='rounded' src={image} alt="" />
+                        <div key={id} className='text-black grid w-[550px]  p-5 shadow-xl mt-4 gap-4 rounded-xl' >
+                            <img className='rounded' src={image} alt={name} />
                             <div className='flex justify-between items-center'>
                                 <p className='font-semibold text-2xl' >{name}</p>
                                 <p>${price}</p>
