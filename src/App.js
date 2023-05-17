25ea2ae67d3953ac958a5678a5e20a35fe87bc53
ranjithkumar404 <ranjithkumarshetty808@gmail.com>
2023-05-17 22:01:54 +0530

    my commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Tour from './Tour';
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const url = 'https://course-api.com/react-tours-project';
+  const [tours, setTours] = useState([]);
+  const myFn = async () => {
+    try {
+      setLoading(true);
+      let res = await fetch(url);
+      let tours = await res.json();
+      setTours(tours);
+      setLoading(false);
+    }
+    catch (er) {
+      console.log(er);
+    }
+  }
+  useEffect(() => {
+    myFn();
+  }, [])
+  return (
+    <div className='flex justify-center items-center' >
+      {loading ? <p>Loading...</p> : <Tour tours={tours}/>}
+    </div>
+  );
+}
+
+
+export default App;
