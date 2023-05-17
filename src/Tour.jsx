a79c5114296a3f112b133225d6bfb8da73b7603c
ranjithkumar404 <ranjithkumarshetty808@gmail.com>
2023-05-17 22:20:05 +0530

    new commit

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,22 +1,13 @@
 import React, { useState } from 'react'
-
+import Tours from './Tours'
 const Tour = ({ tours }) => {
-    const [r, setr] = useState(false)
     return (
         <div>
             {
                 tours.map((i) => {
                     const { id, name, info, image, price } = i;
                     return (
-                        <div key={id} className='text-black grid w-[550px]  p-5 shadow-xl mt-4 gap-4 rounded-xl' >
-                            <img className='rounded' src={image} alt={name} />
-                            <div className='flex justify-between items-center'>
-                                <p className='font-semibold text-2xl' >{name}</p>
-                                <p>${price}</p>
-                            </div>
-                            <p>{r ? info : `${info.substring(0, 200)}... `}<button className='text-lime-500' onClick={() => setr(!r)}> {r ? ' show less' : ' read more'}</button></p>
-
-                        </div>
+                        <Tours key={id} {...tours}/>
                     )
 
 
---
+++ src/Tours.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Tours = ({tour}) => {
+    const [r, setr] = useState(false)
+
+  return (
+    <div>
+         
+          <div key={id} className='text-black grid w-[550px]  p-5 shadow-xl mt-4 gap-4 rounded-xl' >
+              <img className='rounded' src={image} alt={name} />
+              <div className='flex justify-between items-center'>
+                  <p className='font-semibold text-2xl' >{name}</p>
+                  <p>${price}</p>
+              </div>
+              <p>{r ? info : `${info.substring(0, 200)}... `}<button className='text-lime-500' onClick={() => setr(!r)}> {r ? ' show less' : ' read more'}</button></p>
+
+          </div>
+    </div>
+  )
+}
+
+export default Tours
